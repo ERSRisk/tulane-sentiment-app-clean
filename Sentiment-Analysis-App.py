4a63fcb1f2f64cb1b8de4c083a376399fa5079c9
ERSRisk <njenkins4@tulane.edu>
2025-10-06 13:02:21 -0500

    Update Sentiment-Analysis-App.py

--- Sentiment-Analysis-App.py
+++ Sentiment-Analysis-App.py
@@ -1176,7 +1176,8 @@ if selection == "Article Risk Review":
     if status_choice == 'Unreviewed only':
         filtered_df = filtered_df[filtered_df['Reviewed'] != 1]
     elif status_choice == 'Reviewed only':
-        st.dataframe(filtered_df)
+        filtered_df = st.session_state.change_log.drop_duplicates(subset = ['Title'])
+        filtered_df['Published'] = pd.to_datetime(filtered_df['Published'], errors='coerce', utc=True)
 
     start_date = pd.to_datetime(start_date)
     end_date = pd.to_datetime(end_date) + pd.Timedelta(days = 1) - pd.Timedelta(microseconds = 1)
