4ed644beb0a46a34b3f8eab1094ea87b105b9412
ERSRisk <njenkins4@tulane.edu>
2025-10-22 11:04:09 -0500

    Update Sentiment-Analysis-App.py

--- Sentiment-Analysis-App.py
+++ Sentiment-Analysis-App.py
@@ -580,7 +580,15 @@ if selection == "Article Risk Review":
     base_df = st.session_state.articles
     #articles = articles[articles['Published']> start_date.strftime('%Y-%m-%d')]
     #articles = articles[articles['Published']< end_date.strftime('%Y-%m-%d')]
-    filtered_df = base_df[base_df['University Label'] == 1].copy()
+    for col in ['University Label']:
+        if col in st.session_state.articles.columns:
+            st.session_state.articles[col] = (
+            pd.to_numeric(st.session_state.articles[col], errors='coerce')
+            .fillna(0).astype(int)
+        )
+
+    base_df = st.session_state.articles
+    filtered_df = base_df[base_df['University Label'] == 1]
     filtered_df = filtered_df.drop_duplicates(subset=['Title'])
     filtered_df = filtered_df[~(filtered_df['_RiskList'] == 'No Risk')]
     if status_choice == 'Unreviewed only':
